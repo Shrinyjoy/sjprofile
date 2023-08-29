5a256b72da7d2234cffb4db0cbc346e417518cab
shrinyjoy <shrinyjoy@gmail.com>
2023-08-30 04:02:01 +1000

    fourth commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import myfile from "../../src/images/SJ_Resume.docx";
+import myfile from "../../src/images/SJ_Res.docx";
 
 export default class About extends Component {
   render() {
